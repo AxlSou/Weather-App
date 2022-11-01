1c0b2de203b15241ed60d55f1a3ad27109f681c6
AxlSou <axlsoubielle@gmail.com>
2022-11-01 15:25:10 +0000

    Components, stylesheets and icons

---
+++ src/components/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '41c18fdf26msh8b4dc70566cffdcp1c02f4jsnacd95146fdb7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "db0d49b87d88c88376ea8ef526be3e48"
